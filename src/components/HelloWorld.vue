795734c5ebf707006851eb6d5914ef46dfd131ef
lanxinghua <lanxinghua@2dfire.com>
2018-10-27 14:19:01 +0800

    脚手架

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<!-- js -->
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Hello World'
+    }
+  }
+}
+</script>
+
+<!-- CSS -->
+<style scoped>
+h1,{
+  font-weight: normal;
+}
+</style>
